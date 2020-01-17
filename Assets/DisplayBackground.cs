0d5a21e4ca9d7fbe697f182081e3bf00e4680d4e
rubiksmaster02 <therealrubiksmaster@gmail.com>
2020-01-17 14:41:37 -0800

    working on multiple scenes

--- Assets/DisplayBackground.cs
+++ Assets/DisplayBackground.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DisplayBackground : MonoBehaviour
 {
-    [SerializeField] public Image background;
-    
+    public static void Display(RawImage image, string path) 
+    {
+        Texture2D tex = new Texture2D(0, 0);
+        tex.LoadImage(File.ReadAllBytes(path));
+        image.GetComponent<RawImage>().texture = tex;
+    }
 }
--- Assets/MapButton.cs
+++ Assets/MapButton.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,6 +8,7 @@ public class MapButton : MonoBehaviour
 {
     [SerializeField] private RawImage background;
     [SerializeField] private Text mapName;
+    [SerializeField] private Text artist;
     [SerializeField] private Text enemyCount;
     [SerializeField] private Text difficulty;
     [SerializeField] private Text creator;
@@ -16,26 +18,30 @@ public class MapButton : MonoBehaviour
 
     private void Start()
     {
-        mapName.text = map.MetaData.Title;
-        enemyCount.text = "Enemy Count: " + map.Enemies.Count;
-        difficulty.text = "00"; //not implemented yet
-        creator.text = map.MetaData.Creator;
-        
-        var asd = File.ReadAllBytes(@"C:\Users\527251\AppData\Roaming\CircleRhythm\Maps\Map2\bg1.png");
-        Texture2D zxc = new Texture2D(100,100);
-        zxc.LoadRawTextureData(asd);
-        zxc.Apply();
-        background.GetComponent<RawImage>().texture = zxc;
+        try
+        {
+            mapName.text = map.MetaData.Title;
+            enemyCount.text = "Enemy Count: " + map.Enemies.Count;
+            difficulty.text = "00"; //not implemented yet
+            creator.text = map.MetaData.Creator;
+            artist.text = map.MetaData.Artist;
+        }
+        catch (Exception e)
+        {
+            print(e);
+            gameObject.SetActive(false);
+        }
     }
 
     public void Select()
     {
         _selected = true;
+        DisplayBackground.Display(background, Path.Combine(map.Path, map.MetaData.Icon));
     }
     
     public void OnClick()
     {
-        if (!_selected || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Return))
+        if (!_selected || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.Mouse0))
             SceneManager.LoadScene("Game", LoadSceneMode.Single);
         else
             _selected = false;
