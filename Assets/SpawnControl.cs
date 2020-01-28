6aa7811356d3b6d2b6a8fe27c285223db3ae0146
rubiksmaster02 <therealrubiksmaster@gmail.com>
2020-01-28 14:40:29 -0800

    working on spawning

--- Assets/SpawnControl.cs
+++ Assets/SpawnControl.cs
@@ -4,17 +4,38 @@ public class SpawnControl : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
     [SerializeField] private AudioSource audioSource;
+    private Rect _viewportSize;
     private int _currentEnemy;
-    private float _timer;
+    private Map _map;
+
+    private void Start()
+    {
+        _map = MapButton.Map;
+        _viewportSize = GetComponent<RectTransform>().rect;
+    }
 
     private void Update()
     {
-        _timer += Time.deltaTime;
-        if(MapButton.Map.Enemies.Count == _currentEnemy) return;
-        if (_timer > MapButton.Map.Enemies[_currentEnemy].SpawnTime)
+        if (_map.Enemies.Count != _currentEnemy && audioSource.time > _map.Enemies[_currentEnemy].SpawnTime)
         {
-            GameObject enemyInstance = Instantiate(enemy, enemy.transform.parent , false);
-            enemyInstance.transform.localPosition = new Vector2(MapButton.Map.Enemies[_currentEnemy].XLocation,MapButton.Map.Enemies[_currentEnemy].YLocation);
+            GameObject enemyInstance = Instantiate(enemy, enemy.transform.parent, false);
+            var spawnPoint = new Vector2(_map.Enemies[_currentEnemy].XLocation, _map.Enemies[_currentEnemy].YLocation);
+            if (Vector2.Distance(new Vector2(0, 0), new Vector2(spawnPoint.x, 0)) < _viewportSize.width / 2 ||
+                Vector2.Distance(new Vector2(0, 0), new Vector2(0, spawnPoint.y)) < _viewportSize.height / 2)
+            {
+                if (spawnPoint.x / _viewportSize.width < spawnPoint.y / _viewportSize.height)
+                {
+                    if (spawnPoint.x < 0) spawnPoint.x = -1 * _viewportSize.width / 2;
+                    else spawnPoint.x = _viewportSize.width / 2;
+                }
+                else
+                {
+                    if (spawnPoint.y < 0) spawnPoint.y = -1 * _viewportSize.height / 2;
+                    else spawnPoint.y = _viewportSize.height / 2;
+                }
+            }
+
+            enemyInstance.transform.localPosition = spawnPoint;
             enemyInstance.SetActive(true);
             enemyInstance.GetComponent<EnemyControl>().CurrentEnemy = _currentEnemy;
             _currentEnemy++;
