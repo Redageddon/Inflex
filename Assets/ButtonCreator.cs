d35cd2d5a52b53b7c2ed197137a627e07ad5e11d
rubiksmaster02 <therealrubiksmaster@gmail.com>
2020-02-07 12:50:51 -0800

    i actually dont need the creation code

--- Assets/ButtonCreator.cs
+++ Assets/ButtonCreator.cs
@@ -4,67 +4,17 @@ using System.IO;
 
 public class ButtonCreator : MonoBehaviour
 {
+    [SerializeField] private GameObject mapButtonTemp;
     private readonly string[] _mapNames =
        Directory.GetDirectories(System.Environment.ExpandEnvironmentVariables(@"%AppData%\CircleRhythm\Maps\"));
 
-    [SerializeField] 
-    private GameObject mapButtonTemp;
-
-    void Start()
+    private void Start()
     {
-        /*
-        Map mapTest = new Map()
-        {
-            MetaData = new MapMetaData()
-            {
-                Artist = "camellia",
-                Title = "mapTest",
-                Creator = "Rubik",
-                Icon = "img.jpg"
-            },
-            Background = "img.jpg",
-            Lives = 3,
-            Song = "Ghost",
-            EndTime = 10,
-            Enemies = new System.Collections.Generic.List<Enemy>()
-            {
-                new Enemy()
-                {
-                    Rotation = 10,
-                    KillKey = "W",
-                    SpawnTime = 1,
-                    XLocation = 100,
-                    YLocation = 200
-                },
-                new Enemy()
-                {
-                    Rotation = 10,
-                    KillKey = "W",
-                    SpawnTime = 1,
-                    XLocation = 100,
-                    YLocation = 200
-                },
-            },
-            ScreenEvents = new System.Collections.Generic.List<MapScreen>()
-            {
-                new MapScreen()
-                {
-                    Zoom = 10,
-                    RotationSpeed = 10,
-                    SpawnTime = 2
-                },
-                new MapScreen()
-                {
-                    Zoom = 10,
-                    RotationSpeed = 10,
-                    SpawnTime = 2
-                }
-            }
-        };
-        string json = Newtonsoft.Json.JsonConvert.SerializeObject(mapTest, (Newtonsoft.Json.Formatting) 1);
-        File.WriteAllText(Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData),
-            @"CircleRhythm\Maps\test.json"),json);*/
+        CreateMapButtons();
+    }
 
+    private void CreateMapButtons()
+    {
         foreach (string map in _mapNames)
         {
             GameObject button = Instantiate(mapButtonTemp, mapButtonTemp.transform.parent, false);
